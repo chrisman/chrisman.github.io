51e3fa776afd118c77117a8488d7acce13d750d5
Christopher Brown <christopher.p.brown@gmail.com>
2015-11-07 13:14:10 -0700

    page2

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
 $document.ready(function(){
   $('#bio').accordion();
+  var $navElements = $('<li><a href="index.html">home</a></li><li><a href="g18.html">g18</a></li>');
+  $navElements.appendTo('#nav');
 });
