7a257368995c3fbf48a070abb73906183bae4722
Christopher Brown <christopher.p.brown@gmail.com>
2016-04-06 10:58:20 -0600

    :zap: ignore case on keyword sort

--- scripts/services/portfolioservice.js
+++ scripts/services/portfolioservice.js
@@ -3,25 +3,36 @@ app.service('PortfolioService', function($http){
   var portfolio = [];
   var keywordList = [];
 
-  var objectToKeywords = o => o.keywords;
+  var projectToKeywords = o => o.keywords;
   var flattenArray = (acc, curr) => {
     curr.forEach(e => {
       acc.push(e);
     });
     return acc;
   }
+  var toLowerCase = s => s.toLowerCase();
   var uniq = (e, idx, arr) => arr.indexOf(e) === idx;
-  var toLowerCase = e => e.toLowerCase();
+  var compareValues = (x, y) => +(x.value > y.value) || +(x.value === y.value) - 1;
+  var keywordWithIndex = (e, i) => {
+    return {
+      "value": e,
+      "index": i 
+    }
+  };
 
   $http.get('/portfolio.json').then( r => {
 
     portfolio = r.data;
-    keywordList = portfolio
-      .map(objectToKeywords)
+
+    var keywordsUnsorted = portfolio
+      .map(projectToKeywords)
       .reduce(flattenArray, [])
-      .map(toLowerCase)
       .filter(uniq)
-      .sort();
+    var keywordsWithIndex = keywordsUnsorted
+      .map(toLowerCase)
+      .map(keywordWithIndex)
+      .sort(compareValues);
+    keywordList = keywordsWithIndex.map( e => keywordsUnsorted[e.index]);
   });
 
   return {
